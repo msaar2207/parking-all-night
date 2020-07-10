355fa56222846b73e960f45d138b9366dcf539a8
msaar2207 <aun.rizvi@binaryvibes.com>
2020-07-10 20:41:44 +0500

    repositry intialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/pages/home/home.component';
+import { HomeModule } from 'src/pages/home/home.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/components/toolbar/toolbar.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './toolbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [ToolbarComponent]
+})
+export class ToolbarModule { }
