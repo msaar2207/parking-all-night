cfed9c254e06958e7621797ad40cad1231b75ecb
aqibshakeel1992 <aqib_eng@hotmail.com>
2020-07-13 22:41:58 +0500

    Home Page Design Completed

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
-
+  nowDate= new Date();
+  checkIn= new FormControl();
+  checkOut= new FormControl();
   constructor() { }
 
   ngOnInit(): void {
--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
