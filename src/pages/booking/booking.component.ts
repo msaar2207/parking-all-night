fb510a45098077bea25855dfaf5dc4ba6ce8565c
msaar2207 <aun.rizvi@binaryvibes.com>
2020-07-15 18:57:19 +0500

    booking functinality completed

--- src/pages/booking/booking.component.ts
+++ src/pages/booking/booking.component.ts
@@ -11,17 +11,15 @@ export class BookingComponent implements OnInit {
   checkOut = new FormControl();
   nowDate = new Date();
   slots = [
-    { available: 7, booked: 3, total: 10, date: (new Date()).setDate((new Date()).getDate() + 1) },
-    { available: 4, booked: 6, total: 10, date: (new Date()).setDate((new Date()).getDate() + 2) },
-    { available: 8, booked: 2, total: 10, date: (new Date()).setDate((new Date()).getDate() + 3) },
-    { available: 1, booked: 9, total: 10, date: (new Date()).setDate((new Date()).getDate() + 4) },
+    { available: 7, booked: 3, total: 10, selected: 0, date: (new Date()).setDate((new Date()).getDate() + 1) },
+    { available: 4, booked: 6, total: 10, selected: 0, date: (new Date()).setDate((new Date()).getDate() + 2) },
+    { available: 8, booked: 2, total: 10, selected: 0, date: (new Date()).setDate((new Date()).getDate() + 3) },
+    { available: 1, booked: 9, total: 10, selected: 0, date: (new Date()).setDate((new Date()).getDate() + 4) },
   ];
-  totalSelectedSlots = 0;
   carSlotsSelected = [];
   constructor() { }
 
   ngOnInit(): void {
-    this.totalIcons();
   }
   openDate(date) {
     date.open();
@@ -29,26 +27,29 @@ export class BookingComponent implements OnInit {
   updateSelections() {
     this.slots = this.slots.filter(d => d.date >= this.checkIn.value && d.date <= this.checkOut.value);
   }
+  removeSlot(i) {
+    const slot = this.carSlotsSelected[i];
+    slot.selected = 0;
+    const slotIndex = this.slots.findIndex(s => s.date === slot.date);
+    this.slots[slotIndex].available = this.slots[slotIndex].total - this.slots[slotIndex].booked;
+    this.carSlotsSelected.splice(i, 1);
+  }
   minusBooking(i) {
     const slot = this.slots[i];
     slot.available = slot.available + 1;
+    slot.selected = slot.selected - 1;
     this.slots[i] = slot;
-    this.totalSelectedSlots = this.totalSelectedSlots - 1;
-    this.totalIcons();
+    if (!this.carSlotsSelected.find(s => s.date === slot.date)) {
+      this.carSlotsSelected.splice(i, 1);
+    }
   }
   addBooking(i) {
     const slot = this.slots[i];
     slot.available = slot.available - 1;
+    slot.selected = slot.selected + 1;
     this.slots[i] = slot;
-    this.totalSelectedSlots = this.totalSelectedSlots + 1;
-    this.totalIcons();
-  }
-  private totalIcons() {
-    this.carSlotsSelected = [];
-    for (let i = 0; i < this.totalSelectedSlots; i++) {
-      this.carSlotsSelected.push({});
-
+    if (!this.carSlotsSelected.find(s => s.date === slot.date)) {
+      this.carSlotsSelected.splice(i, 0, this.slots[i]);
     }
-    return this.carSlotsSelected;
   }
 }
