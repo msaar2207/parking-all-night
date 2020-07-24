9bb573f375e291d34c6785d29e4c8591e0fb74d7
msaar2207 <aun.rizvi@binaryvibes.com>
2020-07-24 21:16:22 +0500

    toolbar changes

--- src/components/toolbar/toolbar.component.ts
+++ src/components/toolbar/toolbar.component.ts
@@ -6,35 +6,43 @@ import {
   AfterViewInit,
   HostListener,
   Renderer2,
-} from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+} from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: "app-toolbar",
-  templateUrl: "./toolbar.component.html",
-  styleUrls: ["./toolbar.component.scss"],
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit, AfterViewInit {
-  sticky: boolean = false;
+  sticky = false;
   elementPosition: any;
   ismobile = false;
-  @ViewChild("stick") toolbars: ElementRef;
-  constructor(private _renderer: Renderer2, private _bps: BreakpointObserver) {}
+  isHome = false;
+  @ViewChild('stick') toolbars: ElementRef;
+  constructor(private _renderer: Renderer2, private _bps: BreakpointObserver, private router: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
+    this.router.url.subscribe(url => {
+      console.log(url[0], "url path");
+      this.isHome = url[0].path === '';
+    });
     this._bps
       .observe([Breakpoints.Small, Breakpoints.XSmall])
       .subscribe((v) => {
         console.log(v);
-        if (v.matches) this.ismobile = true;
-        else this.ismobile = false;
+        if (v.matches) { this.ismobile = true; }
+        else { this.ismobile = false; }
       });
   }
 
   ngAfterViewInit() {
     this.elementPosition = this.toolbars.nativeElement.offsetTop;
   }
-  @HostListener("window:scroll", ["$event"])
+  @HostListener('window:scroll', ['$event'])
   handleScroll() {
     const windowScroll = window.pageYOffset;
     if (windowScroll >= this.elementPosition) {
