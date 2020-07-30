c0a8bdee6b7c1b80a8fad712a2c8a1ba37fe8f86
msaar2207 <aun.rizvi@binaryvibes.com>
2020-07-30 20:35:23 +0500

    production url Gas APi enums

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  url: 'http://http://18.218.219.122/api/v1/'
+  url: 'http://http://18.218.219.122/api/v1/',
+  Latitude: '83.9207',
+  Longitude: '35.9606',
+  distance: '200',
+  GasAPI: 'rfej9napna'
 };
