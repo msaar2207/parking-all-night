c13132daf570d97d91c350f68eea0bba8313f83d
msaar2207 <aun.rizvi@binaryvibes.com>
2020-07-30 21:01:03 +0500

    production url syntax error free

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://http://18.218.219.122/api/v1/',
+  url: 'http://18.218.219.122/api/v1/',
   Latitude: '83.9207',
   Longitude: '35.9606',
   distance: '200',
